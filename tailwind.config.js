e45433d43e8a72b2d88ec37a098c1092cb69dbae
Skye-LAB <walksky452@gmail.com>
2022-05-22 20:15:15 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        "gruvbox-bg": "#282828", 
+        "gruvbox-bg-1/2": "#32302f", 
+        "gruvbox-bg-1": "#3c3836", 
+        "gruvbox-bg-2": "#504945", 
+        "gruvbox-bg-3": "#665c54", 
+        "gruvbox-bg-4": "#7c6f64", 
+        "gruvbox-fg": "#fbf1c7", 
+        "gruvbox-fg-1": "#ebdbb2", 
+        "gruvbox-fg-2": "#d5c4a1", 
+        "gruvbox-fg-3": "#bdae93", 
+        "gruvbox-fg-4": "#a89984", 
+        "gruvbox-red": "#cc241d",
+        "gruvbox-green": "#98971a",
+        "gruvbox-yellow": "#d79921",
+        "gruvbox-blue": "#458588",
+        "gruvbox-purple": "#b16286",
+        "gruvbox-aqua": "#689d6a",
+        "gruvbox-gray": "#a89984",
+        "gruvbox-orange": "#d65d0e",
+      }
+    },
+  },
+  plugins: [],
+}
