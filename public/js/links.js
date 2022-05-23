123dacac6649840d8999680fc2b92b6082d84ccc
Skye-LAB <walksky452@gmail.com>
2022-05-23 17:49:47 +0700

    change: mengubah dari statis ke dinamik dengan file links.js

--- public/js/links.js
+++ public/js/links.js
@@ -1,1 +1,92 @@
-const links = {};
+const links = [
+  {
+    name: "Linux",
+    color: "green",
+    links: [
+      {
+        name: "DistroTube.com",
+        link: "https://distro.tube",
+      },
+      {
+        name: "Arch Wiki",
+        link: "https://wiki.archlinux.org",
+      },
+      {
+        name: "It's FOSS",
+        link: "https://itsfoss.com/",
+      },
+    ],
+  },
+  {
+    name: "Reddit",
+    color: "orange",
+    links: [
+      {
+        name: "/r/linux",
+        link: "https://www.reddit.com/r/linux",
+      },
+      {
+        name: "/r/unixporn",
+        link: "https://www.reddit.com/r/unixporn",
+      },
+      {
+        name: "/r/voidlinux",
+        link: "https://www.reddit.com/r/voidlinux",
+      },
+    ],
+  },
+  {
+    name: "YouTube",
+    color: "red",
+    links: [
+      {
+        name: "DistroTube",
+        link: "https://www.youtube.com/c/DistroTube",
+      },
+      {
+        name: "WPU",
+        link: "https://www.youtube.com/c/WebProgrammingUNPAS",
+      },
+      {
+        name: "Academind",
+        link: "https://www.youtube.com/c/Academind",
+      },
+    ],
+  },
+  {
+    name: "Programming",
+    color: "blue",
+    links: [
+      {
+        name: "Github",
+        link: "https://www.github.com",
+      },
+      {
+        name: "Gitlab",
+        link: "https://www.gitlab.com",
+      },
+      {
+        name: "Free Code Camp",
+        link: "https://www.freecodecamp.org",
+      },
+    ],
+  },
+  {
+    name: "Google",
+    color: "aqua",
+    links: [
+      {
+        name: "Meet",
+        link: "https://meet.google.com",
+      },
+      {
+        name: "Mail",
+        link: "https://mail.google.com",
+      },
+      {
+        name: "Drive",
+        link: "https://drive.google.com",
+      },
+    ],
+  },
+];
--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,40 @@
+const blocksContainer = document.querySelector("#blocks-container");
+
 const searchEngine = "https://duckduckgo.com/?q=";
 
 const search = (e) => {
   e.preventDefault();
-  const searchContainer = document.getElementById("search-container");
+  const searchContainer = document.querySelector("#search-container");
   const keyword = searchContainer.value;
 
   window.location = searchEngine + keyword;
 };
+
+links.forEach((block) => {
+  let container = document.createElement("div");
+  container.setAttribute("class", "flex flex-col space-y-4 font-fantasque");
+
+  let linksContainer = "";
+  block.links.forEach((links) => {
+    linksContainer += `
+      <a class="hover:text-gruvbox-bg-4" href="${links.link}">
+        ${links.name}
+      </a>
+    `;
+
+    container.innerHTML = `
+      <div
+        class="flex bg-gruvbox-${block.color} justify-center items-center rounded-sm"
+      >
+        <span class="text-gruvbox-fg-1">${block.name}</span>
+      </div>
+      <div
+        class="bg-gruvbox-fg-1 p-1 w-40 h-40 rounded-sm flex flex-col justify-start text-center text-gruvbox-bg-1"
+      >
+      ${linksContainer}
+      </div>
+    `;
+
+    blocksContainer.appendChild(container);
+  });
+});
